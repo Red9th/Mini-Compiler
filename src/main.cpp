c1dfe1088af46cef6200c1be40fc4068ef03d188
Red9th <1961456079@qq.com>
2023-05-15 17:05:14 +0800

    upd: 将 AST 的输出写入文件中

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,8 @@ int main(int argc, const char *argv[]) {
   assert(!ret);
 
   // 输出解析得到的 AST, 其实就是个字符串
+  freopen(output,"w",stdout);
   ast->Dump();
-  cout << endl;
+  fclose(stdout);
   return 0;
 }
